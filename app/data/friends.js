eb6c572a4eafff948231176a3be6b1720c4e08ef
Adowell25 <Adowell25@gmail.com>
2020-07-11 09:12:02 -0500

    restructure

---
+++ app/data/friends.js
@@ -0,0 +1,132 @@
+var friends = [{
+    "name": "Ned Stark",
+    "photo": "https://en.wikipedia.org/wiki/Ned_Stark#/media/File:Ned_Stark-Sean_Bean.jpg",
+    "scores": [
+        1,
+        4,
+        5,
+        4,
+        2,
+        1,
+        4,
+        4,
+        1,
+        1
+    ]
+},
+
+{
+    "name": "Catelyn Stark",
+    "photo": "https://en.wikipedia.org/wiki/Catelyn_Stark#/media/File:Michelle_Fairley_Cat_Stark_in_the_Vale.png",
+    "scores": [
+        4,
+        4,
+        3,
+        3,
+        3,
+        4,
+        1,
+        3,
+        3,
+        2
+    ]
+},
+{
+    "name": "Bran Stark",
+    "photo": "https://en.wikipedia.org/wiki/Bran_Stark#/media/File:Bran_Stark_-_Isaac_Hempstead-Wright.jpeg",
+    "scores": [
+        5,
+        5,
+        2,
+        2,
+        5,
+        5,
+        2,
+        2,
+        3,
+        3
+    ]
+},
+{
+    "name": "Joffrey Baratheon",
+    "photo": "https://en.wikipedia.org/wiki/Joffrey_Baratheon#/media/File:Joffrey_Baratheon-Jack_Gleeson.jpg",
+    "scores": [
+        1,
+        1,
+        3,
+        4,
+        5,
+        4,
+        3,
+        5,
+        4,
+        4
+    ]
+},
+{
+    "name": "Melisandre",
+    "photo": "https://en.wikipedia.org/wiki/Melisandre#/media/File:Melisandre-Carice_van_Houten.jpg",
+    "scores": [
+        5,
+        4,
+        1,
+        3,
+        3,
+        2,
+        3,
+        3,
+        2,
+        1
+    ]
+},
+{
+    "name": "Cersei Lannister",
+    "photo": "https://en.wikipedia.org/wiki/Cersei_Lannister#/media/File:Cersei_Lannister_in_Black_Dress_in_Season_5.jpg",
+    "scores": [
+        2,
+        4,
+        2,
+        1,
+        4,
+        1,
+        5,
+        4,
+        5,
+        5
+    ]
+},
+{
+    "name": "Jon Snow",
+    "photo": "https://en.wikipedia.org/wiki/Jon_Snow_(character)#/media/File:Jon_Snow_Season_8.png",
+    "scores": [
+        3,
+        2,
+        5,
+        4,
+        2,
+        1,
+        1,
+        3,
+        2,
+        1
+    ]
+},
+{
+    "name": "Daenerys Targaryen",
+    "photo": "https://en.wikipedia.org/wiki/Daenerys_Targaryen#/media/File:Daenerys_Targaryen_with_Dragon-Emilia_Clarke.jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        5,
+        1,
+        1,
+        4,
+        5,
+        5,
+        5
+    ]
+}
+];
+
+module.exports = friends
